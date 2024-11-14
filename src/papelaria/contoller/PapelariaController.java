01346fea977a33962634bd00deb3936c3302e6ce
Victoria Lara <victoriap99@gmail.com>
2024-11-14 19:25:09 -0300

    criando a lista

--- src/papelaria/contoller/PapelariaController.java
+++ src/papelaria/contoller/PapelariaController.java
@@ -1,7 +1,11 @@
 package papelaria.contoller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import papelaria.model.Carrinho;
 import papelaria.model.Produto;
+import papelaria.model.ProdutoPapelaria;
 import papelaria.repository.PapelariaRepository;
 
 public class PapelariaController implements PapelariaRepository {
@@ -46,5 +50,34 @@ public class PapelariaController implements PapelariaRepository {
 			System.out.println("Obrigado por comprar conosco!");
 			carrinho.getList().clear();
 		}
+	}
+
+	@Override
+	public List<ProdutoPapelaria> listaProdutos() {
+		List<ProdutoPapelaria> lista = new ArrayList<ProdutoPapelaria>();
+		lista.add(new ProdutoPapelaria(1, "Caderno Espiral", 15.90, "Papel"));
+		lista.add(new ProdutoPapelaria(2, "Caneta Esferográfica", 2.50, "Caneta"));
+        lista.add(new ProdutoPapelaria(3, "Lápis Grafite", 1.20, "Lápis"));
+        lista.add(new ProdutoPapelaria(4, "Bloco de Notas Adesivas", 5.90, "Papel"));
+        lista.add(new ProdutoPapelaria(5, "Caneta Marcador", 3.50, "Marcador"));
+        lista.add(new ProdutoPapelaria(6, "Apontador de Lápis", 1.80, "Acessório"));
+        lista.add(new ProdutoPapelaria(7, "Borracha Branca", 0.90, "Acessório"));
+        lista.add(new ProdutoPapelaria(8, "Corretivo em Fita", 4.30, "Acessório"));
+        lista.add(new ProdutoPapelaria(9, "Tesoura Escolar", 6.50, "Acessório"));
+        lista.add(new ProdutoPapelaria(10, "Régua 30cm", 2.00, "Acessório"));
+        lista.add(new ProdutoPapelaria(11, "Estojo Escolar", 12.90, "Acessório"));
+        lista.add(new ProdutoPapelaria(12, "Lápis de Cor 12 Cores", 8.90, "Lápis"));
+        lista.add(new ProdutoPapelaria(13, "Grampeador Pequeno", 9.90, "Acessório"));
+        lista.add(new ProdutoPapelaria(14, "Folhas Sulfite A4 100 unidades", 14.90, "Papel"));
+        lista.add(new ProdutoPapelaria(15, "Marcador Permanente", 4.90, "Marcador"));
+        lista.add(new ProdutoPapelaria(16, "Envelope Ofício", 1.00, "Papel"));
+        lista.add(new ProdutoPapelaria(17, "Pasta Sanfonada", 7.50, "Organização"));
+        lista.add(new ProdutoPapelaria(18, "Caderneta de Anotações", 6.90, "Papel"));
+        lista.add(new ProdutoPapelaria(19, "Compasso Escolar", 5.80, "Acessório"));
+        lista.add(new ProdutoPapelaria(20, "Marcador de Texto", 3.20, "Marcador"));
+        lista.add(new ProdutoPapelaria(21, "Post-it 76x76mm", 5.00, "Papel"));
+        lista.add(new ProdutoPapelaria(22, "Canetinhas Hidrocor 12 Cores", 9.90, "Caneta"));
+        lista.add(new ProdutoPapelaria(23, "Caderno de Desenho", 10.50, "Papel"));
+		return lista;
 	}
 }
--- src/papelaria/repository/PapelariaRepository.java
+++ src/papelaria/repository/PapelariaRepository.java
@@ -1,6 +1,9 @@
 package papelaria.repository;
 
+import java.util.List;
+
 import papelaria.model.Produto;
+import papelaria.model.ProdutoPapelaria;
 
 public interface PapelariaRepository {
 
@@ -8,4 +11,5 @@ public interface PapelariaRepository {
 	public void adicionarNoCarrinho(Produto produto);
 	public void exibirTotal(); 
 	public void finalizarPagamento();
+	public List<ProdutoPapelaria> listaProdutos();
 }
